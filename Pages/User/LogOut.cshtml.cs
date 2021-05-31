84472ec018d6cd16232f6a14ac011129653e12c2
johnsjoo <70665563+johnsjoo@users.noreply.github.com>
2021-05-31 13:15:42 +0200

    Style, feat
    Feat
    You get a response messege if you have put in wrong username , password or if account does not exists.
    The user now have a logout button to easaly logout spec user.
    
    Style
    Red alert text when you as a user put in incorrect username or password.

---
+++ Pages/User/LogOut.cshtml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SNACKIS___Webb.Tools;
+
+namespace SNACKIS___Webb.Pages.User
+{
+    public class LogOutModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+
+            ToolBox.ActiveRole = null;
+            TokenChecker.UserStatus = false;
+            HttpContext.Session.Clear();
+
+            return RedirectToPage("/index");
+        }
+    }
+}
