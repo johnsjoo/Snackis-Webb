2f9c0ad88fc3b0ffece86d372eb278872b317975
Azamat Bayev <azamat.bayev1@gmail.com>
2021-06-10 12:26:49 +0200

    Added chatt view, small changes to css/stly
    Changes to style on index page.
    Added a chat view for users.

---
+++ Pages/User/chatt.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SNACKIS___Webb.Pages.User
+{
+    public class chattModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
