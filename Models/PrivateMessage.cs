bce2dc801ddf575c094a23232f9497b2611573d1
johnsjoo <70665563+johnsjoo@users.noreply.github.com>
2021-06-10 23:12:42 +0200

    Feat
    Feat
    Messages works now so you can send private messages to another user.
    Note: Need to fix that users can send a message to a user they never chatted with before.

--- Models/PrivateMessage.cs
+++ Models/PrivateMessage.cs
@@ -6,15 +6,16 @@ using System.Threading.Tasks;
 
 namespace SNACKIS___Webb.Models
 {
-    public class Message
+    public class PrivateMessage
     {
-
         [JsonPropertyName("id")]
         public string Id { get; set; }
         [JsonPropertyName("userid")]
         public string UserId { get; set; }
+
         [JsonPropertyName("message")]
-        public string message { get; set; }
+        public string Message { get; set; }
+
         [JsonPropertyName("date")]
         public DateTime Date { get; set; }
         [JsonPropertyName("messagereceiver")]
