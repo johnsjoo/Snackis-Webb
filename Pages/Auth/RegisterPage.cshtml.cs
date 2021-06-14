307b99b0fc1b544c3ef305290c8a4a591195760a
johnsjoo <70665563+johnsjoo@users.noreply.github.com>
2021-06-14 16:30:41 +0200

    Feat,Style
    Feat
    Added javascript to toggle accept GDPR info.
    Style
    Fix styling, buttons in admin page fixed.

--- Pages/Auth/RegisterPage.cshtml.cs
+++ Pages/Auth/RegisterPage.cshtml.cs
@@ -44,16 +44,13 @@ namespace SNACKIS___Webb.Pages.Auth
 
         public async Task<IActionResult> OnPostAsync()
         {
-            //Behöver Depency injection refactory
+            
             if (UploadFile != null)
             {
                 var file = "./wwwroot/img/" + UploadFile.FileName;
-
-                using (var filestream = new FileStream(file, FileMode.Create))
-                {
-                    NewUser.Image = UploadFile.FileName;
-                    await UploadFile.CopyToAsync(filestream);
-                }
+                NewUser.Image = UploadFile.FileName;
+                var filestream = new FileStream(file, FileMode.Create);
+                await UploadFile.CopyToAsync(filestream);
             }
             await _authGateway.RegisterNewUser(NewUser);
             return RedirectToPage("/Index");  
